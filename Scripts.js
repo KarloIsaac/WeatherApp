b9a85c45ef96109a3a87734d8475a6aba2667e11
KarloIsaac <qafqunam2001@gmail.com>
2017-11-16 09:19:42 -0600

    Now the app also displays location, temperature (including minimum and maximum) and weather icon

--- Scripts.js
+++ Scripts.js
@@ -13,11 +13,17 @@ function updateWeather() {
 
 
 function processCoordinates(latitude, longitude) {
+    updateWeatherIformation(latitude, longitude);
+    updateLocation(latitude, longitude);
+}
+
+
+function updateWeatherIformation(latitude, longitude) {
     var xmlHttpRequest  = new XMLHttpRequest();
     xmlHttpRequest.onreadystatechange = function() {
         if(xmlHttpRequest.readyState === 4 && xmlHttpRequest.status == 200) {
             var jsonResponse = JSON.parse(xmlHttpRequest.responseText);
-            processJsonResponse(jsonResponse);
+            diplayWeatherInformationResponse(jsonResponse);
         }
     }
     var url = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
@@ -26,7 +32,34 @@ function processCoordinates(latitude, longitude) {
 }
 
 
-function processJsonResponse(jsonResponse) {
+function diplayWeatherInformationResponse(jsonResponse) {
     var weather = jsonResponse.weather[0];
-    console.log(weather.description);
+    document.getElementById("weather-description").innerHTML = weather.description;
+    document.getElementById("weather-image").src = weather.icon;
+    var temperatureInfo = jsonResponse.main;
+    document.getElementById("temp").innerHTML = temperatureInfo["temp"];
+    document.getElementById("min-temp").innerHTML = temperatureInfo["temp_min"];
+    document.getElementById("max-temp").innerHTML = temperatureInfo["temp_max"];
+}
+
+
+function updateLocation(latitude, longitude) {
+    var xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange = function() {
+        if(xmlHttpRequest.readyState === 4 && xmlHttpRequest.status == 200) {
+            jsonResponse = JSON.parse(xmlHttpRequest.responseText);
+            diplayLocationInformationResponse(jsonResponse);
+        }
+    }
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?result_type=locality&latlng=" +
+            latitude + "," + longitude + "&sensor=true&key=AIzaSyC9FJPGJX7FLhjybKWBmr3hWkePo1Wfz24"
+    xmlHttpRequest.open("GET", url);
+    xmlHttpRequest.send();
+}
+
+
+function diplayLocationInformationResponse(jsonResponse) {
+    var results = jsonResponse.results[0];
+    var location = results.formatted_address;
+    document.getElementById("location").innerHTML = location;
 }
